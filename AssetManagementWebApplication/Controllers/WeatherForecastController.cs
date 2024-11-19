c8b8cdd86a37bb8ff92e09bd324b9f0502ed54fd
Shikhar <iamshikhartiwari@gmail.com>
2024-11-19 15:47:53 +0530

    testing the working

--- AssetManagementWebApplication/Controllers/WeatherForecastController.cs
+++ AssetManagementWebApplication/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AssetManagementWebApplication.Controllers
 	{
 		private static readonly string[] Summaries = new[]
 		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Calm"
 		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
